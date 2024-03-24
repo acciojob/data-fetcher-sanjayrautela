dc9afb4d52d38a7a9f4edea8d08a6a87971dafb1
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-03-24 06:52:10 +0000

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import DataFetcher from "./DataFetcher";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Data Fetched from API</h1>
+        <DataFetcher></DataFetcher>
     </div>
   )
 }
